3d0f75f095b5f9632fe31d6a42c1d809f7dee586
disha <dishanayak395@gmail.com>
2026-06-29 21:36:41 +0530

    first commit

---
+++ uni-admit-backend/uni-admit-bom/src/main/java/com/example/uni_admit_bom/UniAdmitBomApplication.java
@@ -0,0 +1,13 @@
+package com.example.uni_admit_bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniAdmitBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniAdmitBomApplication.class, args);
+	}
+
+}
